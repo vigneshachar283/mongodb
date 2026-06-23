7d9609f0c2498c00e4ecb9efe9c96d4979a77ca2
Vignesh <vignesh24cs@rnsit.ac.in>
2026-06-23 10:17:55 +0530

    new commit

---
+++ models/menu.js
@@ -0,0 +1,15 @@
+const  mongoose=require("mongoose");
+
+const menuSchema = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    }
+})
+
+module.exports=mongoose.model('Menu',menuSchema);
--- server.js
+++ server.js
@@ -3,29 +3,44 @@ const app=express();
 const db=require("./db");
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
-const Person=require("./models/person");
+const person=require("./models/menu");
 
-app.get("/",async(req,res)=>{
+
+app.get("/",(req,res)=>{
     res.send("hello world");
 })
 
-app.post("/person",(req,res)=>{
+app.post("/person",async(req,res)=>{
    
     try{
 
     const data=req.body;
     const newperson = new person(data);
 
-    const response=await newperson.save();
+    const response= await newperson.save();
      console.log("data saved ");
      res.status(200).json(response);
 
     }catch(err){
 
-
+       console.log(err);
+       res.status(500).json({error:"internal server error"});
     }
         
     });
+
+    app.get("/person",async(req,res)=>{
+try{
+        const data=await person.find();
+        console.log("data fetched");
+        res.status(200).json(data);
+    
+    }
+        catch(err){
+            console.log(err);
+            res.status(500).json({error:"internal server error"});
+        }
+    }    )
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
